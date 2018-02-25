90c854b0710146fa073141ea6a969f349edb0d52
Rohan <rohanraarora@gmail.com>
2018-02-25 11:31:17 +0530

    Add comment in my-branch

--- app/src/main/java/com/forkthecode/gitdemo/MainActivity.java
+++ app/src/main/java/com/forkthecode/gitdemo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"Hello World",Toast.LENGTH_LONG).show();
-
+        //My branch
 
 
     }
